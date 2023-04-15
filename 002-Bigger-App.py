1bcb8ef9cc2ef20107a91b2793f6abec27dc288e
Guilherme Silva <guilhermebaos@gmail.com>
2023-04-15 12:08:55 +0100

    Flask POST e GET

--- 002-Bigger-App.py
+++ 002-Bigger-App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 print(app)
@@ -19,3 +19,24 @@ def market_page():
         {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
     ]
     return render_template("market.html", items=items)
+
+
+# Based on this video: https://www.youtube.com/watch?v=9MHYHgh4jYc
+# State that this route can use "GET" and "POST" methods, the default is only GET
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST": 
+        user = request.form["name"]
+        return render_template("login.html", usr=user)
+    else:
+        return render_template("login.html", usr=False)
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>Hello, {usr}!</h1>"
+
+
+@app.route("/lists")
+def lists():
+    return render_template("lists.html")
